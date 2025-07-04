31e0ddd90babd6e891c6cf97f53cf4f324b64768
Pig Fang <g-plane@hotmail.com>
2025-07-04 19:03:19 +0800

    feat: support formatting expr call

--- pretty_jinja/src/printer.rs
+++ pretty_jinja/src/printer.rs
@@ -21,9 +21,9 @@ struct Ctx<'b> {
 
 fn print_node(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
     match node.kind() {
-        SyntaxKind::ARG => todo!(),
+        SyntaxKind::ARG => print_arg(node, ctx),
         SyntaxKind::EXPR_BIN => print_expr_bin(node, ctx),
-        SyntaxKind::EXPR_CALL => todo!(),
+        SyntaxKind::EXPR_CALL => print_expr_call(node, ctx),
         SyntaxKind::EXPR_CONCAT => print_expr_concat(node, ctx),
         SyntaxKind::EXPR_DICT => print_expr_dict(node, ctx),
         SyntaxKind::EXPR_DICT_ITEM => print_expr_dict_item(node, ctx),
@@ -52,6 +52,18 @@ fn print_node(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
     }
 }
 
+fn print_arg(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
+    Doc::list(
+        node.children_with_tokens()
+            .filter(|element| element.kind() != SyntaxKind::WHITESPACE)
+            .map(|element| match element {
+                NodeOrToken::Node(node) => print_node(&node, ctx),
+                NodeOrToken::Token(token) => Doc::text(token.text().to_string()),
+            })
+            .collect(),
+    )
+}
+
 fn print_expr_bin(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
     let doc = print_expr_with_operator(node, ctx);
     if node
@@ -64,13 +76,27 @@ fn print_expr_bin(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
     }
 }
 
+fn print_expr_call(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
+    node.first_child()
+        .map(|node| print_node(&node, ctx))
+        .unwrap_or_else(Doc::nil)
+        .append(print_comma_separated_with_delimiter(
+            node.children_with_tokens()
+                .skip_while(|node_or_token| node_or_token.kind() != SyntaxKind::L_PAREN),
+            ctx,
+            None,
+            None,
+            false,
+        ))
+}
+
 fn print_expr_concat(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
     print_expr_with_operator(node, ctx).group()
 }
 
 fn print_expr_dict(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
     print_comma_separated_with_delimiter(
-        node,
+        node.children_with_tokens(),
         ctx,
         ctx.options.expr_dict_trailing_comma,
         ctx.options.expr_dict_prefer_single_line,
@@ -115,7 +141,7 @@ fn print_expr_ident(node: &SyntaxNode) -> Doc<'static> {
 
 fn print_expr_list(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
     print_comma_separated_with_delimiter(
-        node,
+        node.children_with_tokens(),
         ctx,
         ctx.options.expr_list_trailing_comma,
         ctx.options.expr_list_prefer_single_line,
@@ -147,7 +173,7 @@ fn print_expr_test(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
 
 fn print_expr_tuple(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
     print_comma_separated_with_delimiter(
-        node,
+        node.children_with_tokens(),
         ctx,
         if node.children().count() == 1 {
             Some(TrailingComma::Always)
@@ -205,19 +231,21 @@ fn print_root_expr(node: &SyntaxNode, ctx: &Ctx) -> Doc<'static> {
 }
 
 fn print_comma_separated_with_delimiter(
-    node: &SyntaxNode,
+    elements: impl Iterator<Item = NodeOrToken>,
     ctx: &Ctx,
     trailing_comma: Option<TrailingComma>,
     prefer_single_line: Option<bool>,
     delim_spacing: bool,
 ) -> Doc<'static> {
     Doc::list(
-        node.children_with_tokens()
+        elements
             .filter(|element| element.kind() != SyntaxKind::WHITESPACE)
             .map(|element| match element {
                 NodeOrToken::Node(child) => {
                     let doc = print_node(&child, ctx);
-                    let doc = if node.last_child().is_some_and(|last| last == child) {
+                    let doc = if child.next_sibling().is_some() {
+                        doc.append(Doc::text(",")).append(Doc::line_or_space())
+                    } else {
                         let comma = match trailing_comma.unwrap_or(ctx.options.trailing_comma) {
                             TrailingComma::Never => Doc::nil(),
                             TrailingComma::Always => Doc::text(","),
@@ -226,8 +254,6 @@ fn print_comma_separated_with_delimiter(
                             }
                         };
                         doc.append(comma)
-                    } else {
-                        doc.append(Doc::text(",")).append(Doc::line_or_space())
                     };
                     doc.nest(ctx.indent_width)
                 }
@@ -236,29 +262,31 @@ fn print_comma_separated_with_delimiter(
                         Doc::nil()
                     } else {
                         let doc = Doc::text(token.text().to_string());
-                        if token.index() == 0 {
-                            let ws = if !prefer_single_line
-                                .unwrap_or(ctx.options.prefer_single_line)
-                                && token.next_token().is_some_and(|token| {
-                                    token.kind() == SyntaxKind::WHITESPACE
-                                        && token.text().contains('\n')
-                                }) {
-                                Doc::hard_line()
-                            } else if delim_spacing {
-                                Doc::line_or_space()
-                            } else {
-                                Doc::line_or_nil()
-                            };
-                            doc.append(ws).nest(ctx.indent_width)
-                        } else if node.last_token() == Some(token) {
-                            let ws = if delim_spacing {
-                                Doc::line_or_space()
-                            } else {
-                                Doc::line_or_nil()
-                            };
-                            ws.append(doc)
-                        } else {
-                            doc
+                        match token.kind() {
+                            SyntaxKind::L_PAREN | SyntaxKind::L_BRACKET | SyntaxKind::L_BRACE => {
+                                let ws = if !prefer_single_line
+                                    .unwrap_or(ctx.options.prefer_single_line)
+                                    && token.next_token().is_some_and(|token| {
+                                        token.kind() == SyntaxKind::WHITESPACE
+                                            && token.text().contains('\n')
+                                    }) {
+                                    Doc::hard_line()
+                                } else if delim_spacing {
+                                    Doc::line_or_space()
+                                } else {
+                                    Doc::line_or_nil()
+                                };
+                                doc.append(ws).nest(ctx.indent_width)
+                            }
+                            SyntaxKind::R_PAREN | SyntaxKind::R_BRACKET | SyntaxKind::R_BRACE => {
+                                let ws = if delim_spacing {
+                                    Doc::line_or_space()
+                                } else {
+                                    Doc::line_or_nil()
+                                };
+                                ws.append(doc)
+                            }
+                            _ => doc,
                         }
                     }
                 }
